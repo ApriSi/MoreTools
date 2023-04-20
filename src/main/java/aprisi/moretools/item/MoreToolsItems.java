8d551a94963b1aa5d2176d7f50400938d1124585
Patrick From <Patrickfromgnomedk@gmail.com>
2023-04-20 23:08:01 +0200

    Dirt Tools [Axe, Hoe, Sword, Shovel]

--- src/main/java/aprisi/moretools/item/MoreToolsItems.java
+++ src/main/java/aprisi/moretools/item/MoreToolsItems.java
@@ -10,16 +10,19 @@ import aprisi.moretools.MoreTools;
 public class MoreToolsItems {
     // Dirt Tools
     public static ToolItem DIRT_PICKAXE = RegisterItem("dirt_pickaxe", new PickaxeBase(DirtToolMaterial.INSTANCE, 1, 2.5f, new Item.Settings()));
+    public static ToolItem DIRT_HOE = RegisterItem("dirt_hoe", new HoeItem(DirtToolMaterial.INSTANCE, 1, 1.0f, new Item.Settings()));
+    public static ToolItem DIRT_AXE = RegisterItem("dirt_axe", new AxeItem(DirtToolMaterial.INSTANCE, 2, 2.5f, new Item.Settings()));
+    public static ToolItem DIRT_SWORD = RegisterItem("dirt_sword", new SwordItem(DirtToolMaterial.INSTANCE, 1, 4.5f, new Item.Settings()));
+    public static ToolItem DIRT_SHOVEL = RegisterItem("dirt_shovel", new ShovelItem(DirtToolMaterial.INSTANCE, 0, 0.3f, new Item.Settings()));
 
-    private static void AddItemsToItemGroup() {
-        AddItemToGroup(ItemGroups.TOOLS, DIRT_PICKAXE);
-    }
+    private static void AddItemsToItemGroup() {}
 
     private static Item RegisterItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(MoreTools.MOD_ID, name), item);
     }
 
     private static ToolItem RegisterItem(String name, ToolItem tool) {
+        AddItemToGroup(ItemGroups.TOOLS, tool);
         return Registry.register(Registries.ITEM, new Identifier(MoreTools.MOD_ID, name), tool);
     }
 
